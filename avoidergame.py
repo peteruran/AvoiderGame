63cc6918f20a223dc01121679ef2978c2f465ac1
Peter Martinus Freyborg Uran <petermfu@gmail.com>
2017-09-27 20:52:23 +0200

    Added comments

--- avoidergame.py
+++ avoidergame.py
@@ -5,6 +5,7 @@ from mobs import *
 
 pygame.init()
 
+# Display settings
 display_width = 800
 display_height = 600
 gameDisplay = pygame.display.set_mode( (display_width, display_height) )
@@ -13,16 +14,19 @@ pygame.display.set_caption('Avoider Game')
 black = (0,0,0)
 white = (255,255,255)
 
+# Set the game clock
 clock = pygame.time.Clock()
 game_over = False
-game_score = 0
 
+# Set mob speeds
 avatar_speed = 5
 enemy_speed = 2
 
-avatar = Avatar((display_width - avatar.width) * 0.5,display_height * 0.8, avatar_speed, "player1.gif")
+# Create the avatar, along with a list to store enemies
+avatar = Avatar(display_width * 0.5, display_height * 0.8, avatar_speed, "player1.gif")
 army = []
 
+# Updates graphics of all mobs, along with despawning enemies
 def update_mobs():
     gameDisplay.blit( avatar.sprite, avatar.get_coordinates() )
 
@@ -31,19 +35,22 @@ def update_mobs():
         gameDisplay.blit( enemy.sprite, enemy.get_coordinates() )
         if enemy.y > display_height:
             army.remove(enemy)
-            game_score += 10
+            #game_score += 10
 
+# Spawning new enemies
 def spawn_mobs():
     if random.random() < 0.1:
         random_x = random.random() * display_width
-        new_enemy = Enemy(random_x, -new_enemy.height, enemy_speed, "enemy1.gif")
+        new_enemy = Enemy(random_x, -20, enemy_speed, "enemy1.gif")
         army.append(new_enemy)
 
+# Main game loop
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
 
+        # Register key presses and refresh coordinates
         keys_pressed = pygame.key.get_pressed()
         x_change = 0; y_change = 0
         if keys_pressed[pygame.K_UP]:
@@ -55,18 +62,19 @@ while not game_over:
         if keys_pressed[pygame.K_RIGHT]:
             x_change = 1
 
+        # Diagonal movement
         if x_change != 0 and y_change != 0:
             x_change *= 0.7
             y_change *= 0.7
 
         avatar.move_a_bit(x_change, y_change)
 
-
+    # Refresh the display, along with updating mobs
     gameDisplay.fill(white)
     spawn_mobs()
     update_mobs()
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(100)
 
 pygame.quit()
 quit()
--- mobs.py
+++ mobs.py
@@ -1,25 +1,32 @@
 import pygame
 
+# Parent class for all mobs
 class Mobile:
     def __init__(self, start_x, start_y, speed, sprite):
+        # Setting initial conditions
         self.x = start_x
         self.y = start_y
 
-        self.speed = 0
+        self.speed = speed
 
+        # Handling sprites
         self.sprite = pygame.image.load(sprite)
-        self.rect = sprite.get_rect()
-        self.width, self.height = rect.get_size()
+        #self.rect = sprite.get_rect()
+        #self.width, self.height = rect.get_size()
 
-    def move_a_bit(self, x, y):
-        self.x += x * self.speed
-        self.y += y * self.speed
+    # Updating mob coordinates
+    def move_a_bit(self, x_change, y_change):
+        self.x += x_change * self.speed
+        self.y += y_change * self.speed
 
+    # Getting coordinates as a tuple
     def get_coordinates(self):
-        return (self.x,self.y)
+        return (self.x, self.y)
 
+# Subclass for the avatar
 class Avatar(Mobile):
     pass
 
+# Subclass for enemies
 class Enemy(Mobile):
     pass
