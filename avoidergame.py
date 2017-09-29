de060e4736f83589a1500664b62981f073b8ff79
Peter Martinus Freyborg Uran <petermfu@gmail.com>
2017-09-29 17:51:10 +0200

    Added collision testing

--- avoidergame.py
+++ avoidergame.py
@@ -23,11 +23,15 @@ game_over = False
 avatar_speed = 5
 enemy_speed = 2
 
+# Handing lists of sprite graphics
+avatar_sprites = ['player1.gif', 'player2.gif']
+enemy_sprites = ['enemy1.gif', 'enemy.gif']
+
 # Ads key repetition, for smooth movement
 pygame.key.set_repeat(60,20)
 
 # Create the avatar, along with a list to store enemies
-avatar = Avatar(display_width * 0.5, display_height * 0.8, avatar_speed, "./sprites/player1.gif")
+avatar = Avatar(display_width * 0.5, display_height * 0.8, avatar_speed, avatar_sprites)
 army = []
 
 # Creates a score board
@@ -35,25 +39,31 @@ game_score = GameScore(text_size=40, text_color=black, x=15, y=15)
 
 # Updates graphics of all mobs, along with despawning enemies and checking for collisions
 def update_mobs():
+    global game_over
     gameDisplay.blit( avatar.sprite, avatar.get_coordinates() )
+    if pygame.time.get_ticks() % 1000 == 0:
+        print("update")
+        avatar.next_sprite()
 
+    # Enemy movement, collision, sprite update and despawning
     for enemy in army:
         enemy.move_a_bit(0,1)
         gameDisplay.blit( enemy.sprite, enemy.get_coordinates() )
+        if pygame.time.get_ticks() % 1000 == 0:
+            avatar.next_sprite()
         if enemy.y > display_height:
             army.remove(enemy)
             game_score.add_to_value(10)
 
         # Checking for collisions with the avatar
-        if enemy.rect.colliderect(avatar.rect):
+        if enemy.is_colliding(avatar):
             game_over = True
-            print("collision")
 
 # Spawning new enemies and adds them to the list
 def spawn_mobs():
     if random.random() < 0.1:
         random_x = random.random() * display_width
-        new_enemy = Enemy(random_x, -20, enemy_speed, "./sprites/enemy1.gif")
+        new_enemy = Enemy(random_x, -20, enemy_speed, enemy_sprites)
         army.append(new_enemy)
 
 # Main game loop
--- mobs.py
+++ mobs.py
@@ -2,7 +2,7 @@ import pygame
 
 # Parent class for all mobs
 class Mobile:
-    def __init__(self, start_x, start_y, speed, sprite):
+    def __init__(self, start_x, start_y, speed, sprites):
         # Setting initial conditions
         self.x = start_x
         self.y = start_y
@@ -10,9 +10,9 @@ class Mobile:
         self.speed = speed
 
         # Handling sprites
-        self.sprite = pygame.image.load(sprite)
-        self.rect = self.sprite.get_rect()
-        self.width, self.height = self.sprite.get_size()
+        self.sprites = sprites
+        self.current_sprite_name = sprites[0]
+        self.next_sprite()
 
     # Updating mob coordinates
     def move_a_bit(self, x_change, y_change):
@@ -22,6 +22,25 @@ class Mobile:
     # Getting coordinates as a tuple
     def get_coordinates(self):
         return (self.x, self.y)
+
+    # Checking for collisions with other mobs
+    def is_colliding(self, mobile):
+        x_collide = False
+        y_collide = False
+        # Check for collision horizontally
+        if self.x < mobile.x < (self.x + self.width) or ( self.x < (mobile.x + mobile.width) < (self.x + self.width) ):
+            x_collide = True
+        # Check for collision vertically
+        if self.y < mobile.y < (self.y + self.height) or ( self.y < (mobile.y + mobile.height) < (self.y + self.height) ):
+            y_collide = True
+        return ( x_collide and y_collide )
+
+    # Load next sprite and update dimensions
+    def next_sprite(self):
+        self.current_sprite_name = self.sprites[ self.sprites.index(self.current_sprite_name) + 1 % (len(self.sprites) - 1) ]
+        self.sprite = pygame.image.load("./sprites/" + str(self.sprites[0]) )
+        self.rect = self.sprite.get_rect()
+        self.width, self.height = self.sprite.get_size()
 
 # Subclass for the avatar
 class Avatar(Mobile):
