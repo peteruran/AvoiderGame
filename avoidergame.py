eb5fe210766cfb00f329a38beeaf19249f20ccd6
Peter Martinus Freyborg Uran <petermfu@gmail.com>
2017-09-29 18:42:46 +0200

    Made AvoiderGame a class

--- avoidergame.py
+++ avoidergame.py
@@ -4,100 +4,97 @@ import math
 from mobs import *
 from gamescore import *
 
-pygame.init()
+class AvoiderGame:
 
-# Display settings
-display_width = 800
-display_height = 600
-gameDisplay = pygame.display.set_mode( (display_width, display_height) )
-pygame.display.set_caption('Avoider Game')
+    def __init__(self, game_display, clock, display_width, display_height):
+        self.game_display = game_display
+        self.clock = clock
+        self.display_width = display_width
+        self.display_height = display_height
 
-black = (0,0,0)
-white = (255,255,255)
+        # Useful colors
+        self.black = (0,0,0)
+        self.white = (255,255,255)
 
-# Set the game clock
-clock = pygame.time.Clock()
-game_over = False
+        # Set the game clock
 
-# Set mob speeds
-avatar_speed = 5
-enemy_speed = 2
+        self.game_over = False
 
-# Handing lists of sprite graphics
-avatar_sprites = ['player1.gif', 'player2.gif']
-enemy_sprites = ['enemy1.gif', 'enemy.gif']
+        # Set mob speeds
+        self.avatar_speed = 5
+        self.enemy_speed = 2
 
-# Ads key repetition, for smooth movement
-pygame.key.set_repeat(60,20)
+        # Handing lists of sprite graphics
+        self.avatar_sprites = ['player1.gif', 'player2.gif']
+        self.enemy_sprites = ['enemy1.gif', 'enemy.gif']
 
-# Create the avatar, along with a list to store enemies
-avatar = Avatar(display_width * 0.5, display_height * 0.8, avatar_speed, avatar_sprites)
-army = []
+        # Ads key repetition, for smooth movement
+        pygame.key.set_repeat(60,20)
 
-# Creates a score board
-game_score = GameScore(text_size=40, text_color=black, x=15, y=15)
+        # Create the avatar, along with a list to store enemies
+        self.avatar = Avatar(self.display_width * 0.5, self.display_height * 0.8, self.avatar_speed, self.avatar_sprites)
+        self.army = []
 
-# Updates graphics of all mobs, along with despawning enemies and checking for collisions
-def update_mobs():
-    global game_over
-    gameDisplay.blit( avatar.sprite, avatar.get_coordinates() )
-    if pygame.time.get_ticks() % 1000 == 0:
-        print("update")
-        avatar.next_sprite()
+        # Creates a score board
+        self.game_score = GameScore(text_size=40, text_color=self.black, x=15, y=15)
 
-    # Enemy movement, collision, sprite update and despawning
-    for enemy in army:
-        enemy.move_a_bit(0,1)
-        gameDisplay.blit( enemy.sprite, enemy.get_coordinates() )
+    # Updates graphics of all mobs, along with despawning enemies and checking for collisions
+    def update_mobs(self):
+        self.game_display.blit( self.avatar.sprite, self.avatar.get_coordinates() )
         if pygame.time.get_ticks() % 1000 == 0:
-            avatar.next_sprite()
-        if enemy.y > display_height:
-            army.remove(enemy)
-            game_score.add_to_value(10)
+            print("update")
+            self.avatar.next_sprite()
 
-        # Checking for collisions with the avatar
-        if enemy.is_colliding(avatar):
-            game_over = True
+        # Enemy movement, collision, sprite update and despawning
+        for enemy in self.army:
+            enemy.move_a_bit(0,1)
+            self.game_display.blit( enemy.sprite, enemy.get_coordinates() )
+            if pygame.time.get_ticks() % 1000 == 0:
+                self.avatar.next_sprite()
+            if enemy.y > self.display_height:
+                self.army.remove(enemy)
+                self.game_score.add_to_value(10)
 
-# Spawning new enemies and adds them to the list
-def spawn_mobs():
-    if random.random() < 0.1:
-        random_x = random.random() * display_width
-        new_enemy = Enemy(random_x, -20, enemy_speed, enemy_sprites)
-        army.append(new_enemy)
+            # Checking for collisions with the avatar
+            if enemy.is_colliding(self.avatar):
+                self.game_over = True
 
-# Main game loop
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+    # Spawning new enemies and adds them to the list
+    def spawn_mobs(self):
+        if random.random() < 0.1:
+            random_x = random.random() * self.display_width
+            new_enemy = Enemy(random_x, -20, self.enemy_speed, self.enemy_sprites)
+            self.army.append(new_enemy)
 
-        # Register key presses and refresh coordinates
-        keys_pressed = pygame.key.get_pressed()
-        x_change = 0; y_change = 0
-        if keys_pressed[pygame.K_UP]:
-             y_change = -1
-        if keys_pressed[pygame.K_DOWN]:
-            y_change = 1
-        if keys_pressed[pygame.K_LEFT]:
-            x_change = -1
-        if keys_pressed[pygame.K_RIGHT]:
-            x_change = 1
+    # Main game loop
+    def on_tick(self):
+        while not self.game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_over = True
 
-        # Diagonal movement
-        if x_change != 0 and y_change != 0:
-            x_change *= 0.7
-            y_change *= 0.7
+                # Register key presses and refresh coordinates
+                keys_pressed = pygame.key.get_pressed()
+                x_change = 0; y_change = 0
+                if keys_pressed[pygame.K_UP]:
+                     y_change = -1
+                if keys_pressed[pygame.K_DOWN]:
+                    y_change = 1
+                if keys_pressed[pygame.K_LEFT]:
+                    x_change = -1
+                if keys_pressed[pygame.K_RIGHT]:
+                    x_change = 1
 
-        avatar.move_a_bit(x_change, y_change)
+                # Diagonal movement
+                if x_change != 0 and y_change != 0:
+                    x_change *= 0.7
+                    y_change *= 0.7
 
-    # Refresh the display, along with updating mobs
-    gameDisplay.fill(white)
-    spawn_mobs()
-    update_mobs()
-    gameDisplay.blit( game_score.get_string(), game_score.get_position() )
-    pygame.display.update()
-    clock.tick(70)
+                self.avatar.move_a_bit(x_change, y_change)
 
-pygame.quit()
-quit()
+            # Refresh the display, along with updating mobs
+            self.game_display.fill(self.white)
+            self.spawn_mobs()
+            self.update_mobs()
+            self.game_display.blit( self.game_score.get_string(), self.game_score.get_position() )
+            pygame.display.update()
---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from avoidergame import *
+
+pygame.init()
+
+# Display settings
+display_width = 800
+display_height = 600
+game_display = pygame.display.set_mode( (display_width, display_height) )
+pygame.display.set_caption('Avoider Game')
+
+clock = pygame.time.Clock()
+
+playscreen = AvoiderGame(game_display, clock, display_width, display_height)
+
+while not playscreen.game_over:
+    playscreen.on_tick()
+    clock.tick(60)
+
+pygame.quit()
+quit()
